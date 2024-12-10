fbd3ac61291104921f5b4a7a53d69766a8791497
hype258 <karol.ogonowski@poczta.onet.eu>
2024-12-10 13:22:50 +0100

    fix

--- Assets/Scripts/BootView.cs
+++ Assets/Scripts/BootView.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 public class BootView : MonoBehaviour
 {
     [SerializeField]
@@ -11,6 +12,8 @@ public class BootView : MonoBehaviour
     [SerializeField] 
     AudioMixer masterMixer;
     
+    [SerializeField] 
+    Slider soundSlider;
     private void Awake()
     {
         DependencyInjectorSystem.BindConfigs();
@@ -18,11 +21,26 @@ public class BootView : MonoBehaviour
         DontDestroyOnLoad(this);
         DontDestroyOnLoad(_eventSystem);
         var load =SceneManager.LoadSceneAsync(Constants.MainMenuScene);
+        SetVolume(PlayerPrefs.GetFloat("SavedMasterVolume", 100));
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
+    
+    public void SetVolume(float _value)
+    {
+        if (_value < 1)
+        {
+            _value = .001f;
+        }
+        
+        PlayerPrefs.SetFloat("SavedMasterVolume", _value);
+        masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
+    }
+    
+    
+    
 
 }

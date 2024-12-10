b84e7585b9b081d70a87a1aa29412d322140d724
hype258 <karol.ogonowski@poczta.onet.eu>
2024-12-10 04:17:32 +0100

    wygrana przegrana fix

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,4 +6,6 @@ public class Constants
     public const int MainMenuScene = 1;
     public const int GameScene = 2;
     public const int SettingsMenu = 3;
+    public const int EndGameLose = 4;
+    public const int EndGameWin = 5;
 }
